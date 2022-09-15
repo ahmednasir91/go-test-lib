4bde88290f18f2885dd1ca72d21e7736acae5020
Ahmed Nasir <ahmednasir91@gmail.com>
2022-09-15 17:30:19 +0500

    init testing lib

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ahmednasir91/go-test-lib
+
+go 1.19
+
+require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
+	github.com/jmoiron/sqlx v1.3.5
+)
---
+++ testing.go
@@ -0,0 +1,30 @@
+package testing
+
+import (
+	"database/sql"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+)
+
+// MockDb is a wrapper around sqlmock and sqlx.DB
+type MockDb struct {
+	mockDB *sql.DB
+	mock   sqlmock.Sqlmock
+	db     *sqlx.DB
+}
+
+// Finish is a wrapper around sqlmock.ExpectationsWereMet
+func (d *MockDb) Finish() {
+	err := d.mockDB.Close()
+	if err != nil {
+		return
+	}
+}
+
+// NewSqlxMockDB creates a new MockDb
+func NewSqlxMockDB() *MockDb {
+	mockDB, mock, _ := sqlmock.New()
+	sqlxDB := sqlx.NewDb(mockDB, "sqlmock")
+	return &MockDb{mockDB, mock, sqlxDB}
+}
